ad8ec25052505054945c84320b9f73e9ac4e5d19
diyorbeksafarov2002 <diyorbeksafarov4@gmail.com>
2024-02-15 20:07:11 +0300

    first commit

--- models/urls.py
+++ models/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('contact-create/', ContactCreateView.as_view()),
-    path('contact/', ContactListView.as_view()),
+    path('/contact-create', ContactCreateView.as_view()),
+    path('/contact', ContactListView.as_view()),
 ]
