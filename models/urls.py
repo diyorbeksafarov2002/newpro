e9e24ae17a0f23ab356089a4d43e3e6771f66c69
diyorbeksafarov2002 <diyorbeksafarov4@gmail.com>
2024-02-22 18:32:47 +0300

    first commit

--- models/urls.py
+++ models/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('/contact-create', ContactCreateView.as_view()),
     path('/contact', ContactListView.as_view()),
+    path('/<int:pk>/delete',DeleteApiView.as_view()),
 ]
--- models/views.py
+++ models/views.py
@@ -1,9 +1,10 @@
-
+from django.http import Http404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import *
 from .models import *
+
 
 class ContactCreateView(APIView):
     def post(self, request, format=None):
@@ -15,9 +16,24 @@ class ContactCreateView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
 class ContactListView(APIView):
     def get(self, request, format=None):
         service = Contact.objects.all()
         serializer = ContactSerializer(service, many=True)
         return Response(serializer.data)
+
+
+class DeleteApiView(APIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+    def get_object(self, pk):
+        try:
+            return Contact.objects.get(pk=pk)
+        except Contact.DoesNotExist:
+            raise Http404
+
+    def delete(self, request, pk, format=None):
+        contact = self.get_object(pk)
+        contact.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
